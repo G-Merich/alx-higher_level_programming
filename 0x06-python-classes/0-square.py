07bd94b11cdb92615e18175bf81b730c5335a548
G-Merich <gmerit2030@gmail.com>
2023-01-25 07:28:22 -0800

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Merit Gloria Ogudu
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+    pass
