5345215f4ef28c38797b698dce1a86737fd06803
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-04-13 18:52:31 -0700

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let argc = process.argv.length;
-if (argc > 3) {
+const argc = process.argv.length;
+if (argc > 4) {
   console.log('Arguments found');
-} else if (argc === 3) {
+} else if (argc === 4) {
   console.log('Argument found');
 } else {
   console.log('No argument');
