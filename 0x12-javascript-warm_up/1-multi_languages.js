bbd329a87ed31302cecf5f5f3420791841a5da67
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-04-13 18:56:41 -0700

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// let firstLine ='C is fun';
-// let secondeLine ='Python is cool';
-// let thirdLine ='JavaScript is amazing';
-
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
