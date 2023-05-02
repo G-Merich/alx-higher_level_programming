1a0ae1df01b980c1d65ee671e7130c228f62130a
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-05-02 03:44:55 -0700

    network

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Python script, POST request to http://0.0.0.0:5000/search_user;
+"""
+
+import requests
+from sys import argv
+if __name__ == "__main__":
+    json = {'q': ""}
+    if len(argv) > 1:
+        json['q'] = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", json)
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        if response.json():
+            print("[{}] {}".format(response.json().get('id'),
+                  response.json().get('name')))
+        else:
+            print("No result")
