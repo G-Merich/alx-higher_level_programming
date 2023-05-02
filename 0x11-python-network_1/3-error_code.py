d48253d835c9680f417e5c2bce6a6e99222d93c8
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-05-02 03:27:47 -0700

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL,
+"""
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as url:
+            print(url.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        error_code = str(e).split(' ')[2][:-1]
+        print("Error code: " + str(error_code))
